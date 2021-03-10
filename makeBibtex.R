403adb22daebe7d49c3f0b40e06d64f667d8d97e
Haututu <taylor.winter@vuw.ac.nz>
2021-03-10 20:45:08 +1300

    testing problems with bibiolgraphy

--- makeBibtex.R
+++ makeBibtex.R
@@ -4,4 +4,8 @@ Sys.getenv("ORCID_TOKEN")
 my_dois <- rorcid::identifiers(rorcid::works("0000-0001-5097-9163"))
 pubs <- rcrossref::cr_cn(dois = my_dois, format = "bibtex")
 
-invisible(lapply(pubs, write, "pubs.bib", append=TRUE))
+pubs <- rorcid::orcid_citations("0000-0001-5097-9163")
+
+writeLines(pubs$citation, "test.bib")
+
+invisible(lapply(pubs, write, "pubs.bib.updated", append=TRUE))
